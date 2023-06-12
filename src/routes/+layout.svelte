8b4fb35ef9d0bf461a4edf2845b6f9ea71b69a24
DanielKvG <d.kooyman19@gmail.com>
2023-06-12 23:50:47 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="scene">
+    <slot/>
+</div>
+
+<style>
+    .scene {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        inset: 0;
+        background-color: lightblue;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<Threlte.Canvas>
+
+    <Threlte.PerspectiveCamera
+        position={{ x:20, y:20, z:20 }}
+    >
+        <Threlte.OrbitControls/>
+    </Threlte.PerspectiveCamera>
+    
+    <Threlte.Mesh
+        geometry={new Three.SphereGeometry(4,64,64)}
+        material={new Three.MeshStandardMaterial({ color: 'white' })}
+    />
+
+    <Threlte.AmbientLight color="white" intensity={0.2} />
+
+    <Threlte.DirectionalLight
+        color="white"
+        intensity={2}
+        position={{ x:10, y:20 }}
+    />
+
+</Threlte.Canvas>
+
+<script lang="ts">
+    import * as Threlte from '@threlte/core'
+    import * as Three from 'three'
+    import * as Utils from 'three/src/math/MathUtils.js'
+</script>
