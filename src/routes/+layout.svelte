a3f2373689a551825d4c4000ad3ff995c877b5ab
DanielKvG <d.kooyman19@gmail.com>
2023-06-13 23:43:42 +0200

    added topbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
+<div class="navbar">
+    <h2>Welcome to My site</h2>
+</div>
 <div class="scene">
-    <slot/>
+    <slot class="three-space"/>
 </div>
 
-<style>
+<style lang="scss">
+    .navbar {
+        display: flex;
+        position: sticky;
+        top: 0;
+        z-index: 3;
+        height: 64px;
+        background-color: #2b2b2b;
+        padding: 0 32px 0 32px;
+    }
+
     .scene {
         width: 100%;
         height: 100%;
